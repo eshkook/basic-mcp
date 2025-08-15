b3ad959ec72e98187c37f6a5975b01fee5f0d75b
eshkook <101457806+eshkook@users.noreply.github.com>
2025-08-15 11:57:41 +0300

    d

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,14 @@ else
     echo "🚀 Running in normal mode (FastAPI)..."
     exec uvicorn app:app --host=0.0.0.0 --port=5000
 fi
+
+
+# to just run a script:
+
+# if [ "$DEBUG" = "true" ]; then
+#     echo "🛠️  Running in DEBUG mode..."
+#     exec python -m debugpy --listen 0.0.0.0:5678 --wait-for-client script.py
+# else
+#     echo "🚀 Running in normal mode..."
+#     exec python script.py
+# fi
