175d699572d23224a198c0156930de1c3982d487
eshkook <101457806+eshkook@users.noreply.github.com>
2025-08-16 13:42:10 +0300

    d

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-if [ "$DEBUG" = "true" ]; then
-    echo "🛠️  Running in DEBUG mode (FastAPI + debugpy)..."
-    exec python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn app:app --host=0.0.0.0 --port=5000 --reload
-else
-    echo "🚀 Running in normal mode (FastAPI)..."
-    exec python -m uvicorn app:app --host=0.0.0.0 --port=5000
-fi
+# if [ "$DEBUG" = "true" ]; then
+#     echo "🛠️  Running in DEBUG mode (FastAPI + debugpy)..."
+#     exec python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn app:app --host=0.0.0.0 --port=5000 --reload
+# else
+#     echo "🚀 Running in normal mode (FastAPI)..."
+#     exec python -m uvicorn app:app --host=0.0.0.0 --port=5000
+# fi
 
 
 # to just run a script:
 
-# if [ "$DEBUG" = "true" ]; then
-#     echo "🛠️  Running in DEBUG mode..."
-#     exec python -m debugpy --listen 0.0.0.0:5678 --wait-for-client script.py
-# else
-#     echo "🚀 Running in normal mode..."
-#     exec python -m script.py
-# fi
+if [ "$DEBUG" = "true" ]; then
+    echo "🛠️  Running in DEBUG mode..."
+    exec python -m debugpy --listen 0.0.0.0:5678 --wait-for-client script
+else
+    echo "🚀 Running in normal mode..."
+    exec python -m script
+fi
