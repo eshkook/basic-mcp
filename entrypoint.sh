c21665cac310a696a10a839531762bfb35e11c46
eshkook <101457806+eshkook@users.noreply.github.com>
2025-08-17 11:44:28 +0300

    d

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-if [ "$DEBUG" = "true" ]; then
-    echo "🛠️  Running in DEBUG mode (FastAPI + debugpy)..."
-    exec python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn app:app --host=0.0.0.0 --port=5000 --reload
+if [ "$MODE" = "script" ]; then
+    if [ "$DEBUG" = "true" ]; then
+        echo "🛠️  Running script in DEBUG mode..."
+        exec python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m script
+    else
+        echo "🛠️  Running script in normal mode..."
+        exec python -m script
+    fi
+
+elif [ "$MODE" = "api" ]; then
+    if [ "$DEBUG" = "true" ]; then
+        echo "🛠️  Running fastapi in DEBUG mode..."
+        exec python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn app:app --host=0.0.0.0 --port=5000 --reload
+    else
+        echo "🚀 Running fastapi in normal mode..."
+        exec python -m uvicorn app:app --host=0.0.0.0 --port=5000
+    fi
+
 else
-    echo "🚀 Running in normal mode (FastAPI)..."
-    exec python -m uvicorn app:app --host=0.0.0.0 --port=5000
+    echo "❌ Unknown MODE: $MODE"
+    exit 1
 fi
 
 
